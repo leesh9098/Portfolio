385b06fffe02497292e2844e09d0c1032c620053
leesh9098 <leesh3578@gmail.com>
2021-05-03 22:46:54 +0800

    Project_NAVER

---
+++ Project_NAVER/JS/script.js
@@ -0,0 +1,24 @@
+function listblue() {
+    document.getElementById('listbtn').style.color = "#0000ff";
+    document.getElementById('gridbtn').style.color = "#000000";
+}
+function gridblue() {
+    document.getElementById('listbtn').style.color = "#000000";
+    document.getElementById('gridbtn').style.color = "#0000ff";
+}
+function setblue() {
+    document.getElementById('listbtn').style.color = "#000000";
+    document.getElementById('gridbtn').style.color = "#000000";
+}
+function subbtn() {
+    document.getElementById('subscribe').style.fontWeight = "bold";
+    document.getElementById('subscribe').style.color = "#000000";
+    document.getElementById('all').style.fontWeight = "normal"
+    document.getElementById('all').style.color = "#777777";
+}
+function allbtn() {
+    document.getElementById('subscribe').style.fontWeight = "normal";
+    document.getElementById('subscribe').style.color = "#777777";
+    document.getElementById('all').style.fontWeight = "bold"
+    document.getElementById('all').style.color = "#000000";
+}
