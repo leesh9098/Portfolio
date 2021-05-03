cd6d5ad55e0d283166bbb175ab75745e43ea54c5
leesh9098 <leesh3578@gmail.com>
2021-05-03 22:49:32 +0800

    Project_B

---
+++ Project_B/Dropdown.js
@@ -0,0 +1,20 @@
+function Down1() {
+    document.getElementById("arrow_down1").style.display = "none";
+    document.getElementById("arrow_up1").style.display = "block";
+    document.getElementById("dropdown1").style.display = "block";
+}
+function Up1() {
+    document.getElementById("arrow_down1").style.display = "block";
+    document.getElementById("arrow_up1").style.display = "none";
+    document.getElementById("dropdown1").style.display = "none";
+}
+function Down2() {
+    document.getElementById("arrow_down2").style.display = "none";
+    document.getElementById("arrow_up2").style.display = "block";
+    document.getElementById("dropdown2").style.display = "block";
+}
+function Up2() {
+    document.getElementById("arrow_down2").style.display = "block";
+    document.getElementById("arrow_up2").style.display = "none";
+    document.getElementById("dropdown2").style.display = "none";
+}
