ce99bb72ff425f13540b5ae9f9959362073ea90e
leesh9098 <leesh9098@naver.com>
2025-11-18 09:45:11 +0900

    chore: add opengraph data

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,20 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '이성훈',
+  title: '이성훈 포트폴리오',
   description: '웹 프론트엔드 개발자',
   verification: {
     google: "9ntG751XLoNKTLA6V_UaAha1i6CmkVetfQhjZGOY4AM",
   },
   other: {
     "naver-site-verification": "793270ec6d77e1db6c89460a17b8b0c2ad302322"
+  },
+  openGraph: {
+    title: "이성훈 포트폴리오",
+    description: "웹 프론트엔드 개발자",
+    images: "/no-image.jpg",
+    url: "https://www.leesh9098.site",
+    siteName: "이성훈 포트폴리오"
   }
 }
 
